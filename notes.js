84cea03ce2c0b72d6076bf6c475a738c29534aaa
sharmadev99800 <devsharma20633@gmail.com>
2025-12-25 14:57:29 +0545

    Create notes.js

---
+++ notes.js
@@ -0,0 +1,21 @@
+const notesData = {
+  memories: {
+    title: "🌸 First Memories",
+    content: `
+      These were the days when everything felt light.
+      Conversations without clocks.
+      Silence that still felt warm.
+      Some people become memories without trying.
+    `
+  },
+
+  unsaid: {
+    title: "🌙 Unsaid Things",
+    content: `
+      There were words I never said.
+      Not because I couldn’t,
+      but because some feelings deserve quiet.
+      If you’re reading this… you already know.
+    `
+  }
+};
