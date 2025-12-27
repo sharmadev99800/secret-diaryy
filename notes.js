eeb79befd41d95e73a13e0f53dcbd48eeb7ab928
sharmadev99800 <devsharma20633@gmail.com>
2025-12-27 10:18:02 +0545

    Update notes.js

--- notes.js
+++ notes.js
@@ -3,14 +3,18 @@ const notesData = {
   memories: {
     title: "🌸 First Memories",
     content: `
-      These were the days when everything felt light.
-      Conversations without clocks.
-      Laughs that didn’t need reasons.
-      Silence that still felt warm.
-
-      Some people don’t leave suddenly.
-      They slowly become memories.
-    `
+      I still remember the days we met 
+      for the first time.
+ . like i never thought that we gonna ever end 
+ like this. but to be honest i never wanted to
+ end this relationship i begged More than 100 
+ times to save this relationship but i dont know how it's 
+ ended , jaba first time hami veteko thyeu ta kati khusi thish 
+ tero akha ma maile herya thhe afu prati mayaa
+ how could i get fooled k, kasle diyo talai eti aat
+ jaba tro khutta nidayo vanthish ma talai chalaunthhe kasari
+ ani ta karaunthish , hamle ek arkha ko jutho samet khaxu sanuu
+ tara maybe tro lagi yo sab normal hola ? haina?? ki kaso? `
   },
 
   unsaid: {
@@ -28,12 +32,20 @@ const notesData = {
   goodbye: {
     title: "🕊 Things I Accepted",
     content: `
-      Not everything needs closure.
-      Some chapters end without explanations.
-      And that’s okay.
-
-      Growth sometimes means letting go
-      without noise.
+      Okay kei xaina tro khusi thyo , taile arulai choose garis
+      even though ma tei waiting list ma huda ni maybe
+      mro ma kei khot thyo , idk who is he tara tesma kei ta ramro
+      dekhe holaas
+      maile tro rpost ni herya thye like u nevrr being loved 
+      tara aile vairaxas re ok ma dherai khusi vye..
+      maile garna nasakeko maya usle gardo raixa i wish he never betrays 
+      you .. ma jhagda grda risaunthhe sayad tesaile gumauna paryo
+      talai , Tara ek palta taile ayera ma sita kura gareko vaye saysd
+      it could sort.. tara khai k decision linxas ghari k ghari k
+      ma tw pagal vai sake ghari rape vyo vanxas ghari bacha xa vanxas
+      ghari dharma mann paryo ghari kkkkkk 
+      sidhai ma chaiyena vaneko vaye vaihalthyo ta..
+      jhut ma jhut kina ho ra khai..
     `
   }
 
